e193dcbc02b24058b65423c18637d21d1ac8184d
ahmed <ahmed.hamdaoui@web-atrio.com>
2023-11-15 13:31:53 +0100

    first-creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonnesComponent } from './personnes/personnes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonnesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/personnes/personnes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personnes',
+  templateUrl: './personnes.component.html',
+  styleUrls: ['./personnes.component.scss']
+})
+export class PersonnesComponent {
+
+}
